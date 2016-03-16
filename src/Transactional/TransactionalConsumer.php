e7e00d4ab4705e9a4f6af64c6b25a5bff059c7d1
remi-san <remi.san@gmail.com>
2016-03-16 20:52:08 +0000

    Added a transactional consumer

---
+++ src/Transactional/TransactionalConsumer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Burrow\Transactional;
+
+use Burrow\Exception\ConsumerException;
+use Burrow\QueueConsumer;
+use RemiSan\TransactionManager\Exception\BeginException;
+use RemiSan\TransactionManager\Transactional;
+
+class TransactionalConsumer
+{
+    /**
+     * @var QueueConsumer
+     */
+    private $consumer;
+
+    /**
+     * @var Transactional
+     */
+    private $transactionManager;
+
+    /**
+     * Constructor
+     *
+     * @param QueueConsumer $consumer
+     * @param Transactional $transactionManager
+     */
+    public function __construct(QueueConsumer $consumer, Transactional $transactionManager)
+    {
+        $this->consumer = $consumer;
+        $this->transactionManager = $transactionManager;
+    }
+
+    /**
+     * Consumes a message.
+     *
+     * @param  string $message
+     *
+     *
+     * @throws \Exception
+     */
+    public function consume($message)
+    {
+        try {
+            $this->transactionManager->beginTransaction();
+        } catch (BeginException $e) {
+            throw new ConsumerException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        try {
+            $this->consumer->consume($message);
+            $this->transactionManager->commit();
+        } catch (\Exception $e) {
+            $this->transactionManager->rollback();
+            throw $e;
+        }
+    }
+}
