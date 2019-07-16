df47e38465f25128ef6cf9db348addd15df1966d
Pol-Valentin Cami <polvalentin@evaneos.com>
2019-07-16 10:36:04 +0200

    fix: QueuePublishingEventBus can not extends final class so we inject it instead of extending it

--- src/Broadway/QueuePublishingEventBus.php
+++ src/Broadway/QueuePublishingEventBus.php
@@ -1,36 +1,39 @@
 <?php
+
 namespace Burrow\Broadway;
 
 use Broadway\Domain\DomainEventStream;
+use Broadway\EventHandling\EventBus;
+use Broadway\EventHandling\EventListener;
 use Broadway\EventHandling\SimpleEventBus;
 use Burrow\QueuePublisher;
 
-class QueuePublishingEventBus extends SimpleEventBus
+class QueuePublishingEventBus implements EventBus
 {
     /** @var DomainEventStream[] */
     private $queue;
-
     /** @var bool */
     private $isPublishing;
-
     /** @var DomainEventStreamSerializer */
     private $serializer;
-
     /** @var QueuePublisher */
     private $queuePublisher;
+    /** @var SimpleEventBus */
+    private $eventBus;
 
-    /**
-     * Constructor
-     *
-     * @param DomainEventStreamSerializer $serializer
-     * @param QueuePublisher              $queuePublisher
-     */
-    public function __construct(DomainEventStreamSerializer $serializer, QueuePublisher $queuePublisher)
-    {
+    public function __construct(
+        DomainEventStreamSerializer $serializer,
+        QueuePublisher $queuePublisher,
+        EventBus $eventBus = null
+    ) {
+        if ($eventBus === null) {
+            $eventBus = new SimpleEventBus();
+        }
         $this->queue = [];
         $this->isPublishing = false;
         $this->serializer = $serializer;
         $this->queuePublisher = $queuePublisher;
+        $this->eventBus = $eventBus;
     }
 
     /**
@@ -43,11 +46,11 @@ class QueuePublishingEventBus extends SimpleEventBus
     {
         $this->queue[] = $domainMessages;
 
-        if (! $this->isPublishing) {
+        if (!$this->isPublishing) {
             $this->isPublishing = true;
 
             try {
-                parent::publish($domainMessages);
+                $this->eventBus->publish($domainMessages);
 
                 while ($domainMessageStream = array_shift($this->queue)) {
                     $serializedDomainMessages = $this->serializer->serialize($domainMessages);
@@ -60,5 +63,10 @@ class QueuePublishingEventBus extends SimpleEventBus
                 $this->isPublishing = false;
             }
         }
+    }
+
+    public function subscribe(EventListener $eventListener)
+    {
+        $this->eventBus->subscribe($eventListener);
     }
 }
--- tests/unit/Broadway/QueuePublishingEventBusTest.php
+++ tests/unit/Broadway/QueuePublishingEventBusTest.php
@@ -6,31 +6,38 @@ use Broadway\Domain\DateTime;
 use Broadway\Domain\DomainEventStream;
 use Broadway\Domain\DomainMessage;
 use Broadway\Domain\Metadata;
+use Broadway\EventHandling\EventBus;
+use Broadway\EventHandling\EventListener;
+use Broadway\EventHandling\SimpleEventBus;
 use Burrow\Broadway\DomainEventStreamSerializer;
 use Burrow\Broadway\QueuePublishingEventBus;
 use Burrow\QueuePublisher;
+use Mockery;
 
 class QueuePublishingEventBusTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var DomainEventStreamSerializer
-     */
+    /** @var DomainEventStreamSerializer|\Mockery\MockInterface */
     private $serializer;
-
-    /**
-     * @var QueuePublisher
-     */
+    /** @var QueuePublisher|\Mockery\MockInterface */
     private $queuePublisher;
+    /** @var SimpleEventBus|\Mockery\MockInterface */
+    private $eventBus;
+
+    /** @var QueuePublishingEventBus */
+    private $sut;
 
     protected function tearDown()
     {
-        \Mockery::close();
+        Mockery::close();
     }
 
     protected function setUp()
     {
-        $this->serializer = \Mockery::mock(DomainEventStreamSerializer::class);
-        $this->queuePublisher = \Mockery::mock(QueuePublisher::class);
+        $this->serializer = Mockery::mock(DomainEventStreamSerializer::class);
+        $this->queuePublisher = Mockery::mock(QueuePublisher::class);
+        $this->eventBus = Mockery::mock(EventBus::class);
+
+        $this->sut = new QueuePublishingEventBus($this->serializer, $this->queuePublisher, $this->eventBus);
     }
 
     /**
@@ -39,7 +46,7 @@ class QueuePublishingEventBusTest extends \PHPUnit_Framework_TestCase
     public function it_should_publish_in_queue()
     {
         $stream = new DomainEventStream([
-            new DomainMessage('a', 0, new Metadata(), [], DateTime::now())
+            new DomainMessage('a', 0, new Metadata(), [], DateTime::now()),
         ]);
 
         $this->serializer->shouldReceive('serialize')
@@ -50,8 +57,11 @@ class QueuePublishingEventBusTest extends \PHPUnit_Framework_TestCase
             ->with('serialized')
             ->once();
 
-        $eventBus = new QueuePublishingEventBus($this->serializer, $this->queuePublisher);
-        $eventBus->publish($stream);
+        $this->eventBus->shouldReceive('publish')
+            ->with($stream)
+            ->once();
+
+        $this->sut->publish($stream);
     }
 
     /**
@@ -60,19 +70,31 @@ class QueuePublishingEventBusTest extends \PHPUnit_Framework_TestCase
     public function it_should_throw_the_exception_if_it_catches_one()
     {
         $stream = new DomainEventStream([
-            new DomainMessage('a', 0, new Metadata(), [], DateTime::now())
+            new DomainMessage('a', 0, new Metadata(), [], DateTime::now()),
         ]);
 
         $this->serializer->shouldReceive('serialize')
-             ->with($stream)
-             ->andReturn('serialized');
+            ->with($stream)
+            ->andReturn('serialized');
 
         $this->queuePublisher->shouldReceive('publish')
-             ->andThrow(\Exception::class);
+            ->andThrow(\Exception::class);
 
         $this->setExpectedException(\Exception::class);
 
-        $eventBus = new QueuePublishingEventBus($this->serializer, $this->queuePublisher);
-        $eventBus->publish($stream);
+        $this->sut->publish($stream);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_subscribe_event_listener()
+    {
+        $eventListener = Mockery::mock(EventListener::class);
+
+        $this->eventBus->shouldReceive('subscribe')
+            ->with($eventListener);
+
+        $this->sut->subscribe($eventListener);
     }
 }
